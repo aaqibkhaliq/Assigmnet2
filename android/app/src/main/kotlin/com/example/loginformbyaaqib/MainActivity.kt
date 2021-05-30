808dc3b2dca67fce711493e192518b4f7605cb76
Aaqib Khalique <aaqibkhaliq2@gmail.com>
2021-05-30 22:22:30 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/loginformbyaaqib/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.loginformbyaaqib
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Login Page"),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 60,
+              ),
+              Container(
+                width: 200,
+                child: Center(
+                  child: Text("NAME"),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 200,
+                child: TextField(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 200,
+                child: Center(
+                  child: Text("Password"),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 200,
+                child: TextField(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Login"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
